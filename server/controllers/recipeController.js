370de92224f84a7420a9200ccaa59e748f022510
PrathamModi001 <prathammodi001@gmail.com>
2023-04-11 18:51:56 +0530

    small image uploader change

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -115,36 +115,48 @@ exports.getSubmit = (req, res, next) => {
     })
 }
 
-exports.postSubmit = (req, res, next) => {
-    let sampleFile;
-    let uploadPath;
-    let newImageName;
+exports.postSubmit = async (req, res) => {
+    try {
 
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
+        let imageUploadFile;
+        let uploadPath;
+        let newImageName;
+
+        if (!req.files || Object.keys(req.files).length === 0) {
+            console.log('No Files where uploaded.');
+        } else {
+
+            imageUploadFile = req.files.image;
+            newImageName = Date.now() + imageUploadFile.name;
+
+            uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
+
+            imageUploadFile.mv(uploadPath, function (err) {
+                if (err) return res.satus(500).send(err);
+            })
+
+        }
+
+        const newRecipe = new Recipe({
+            name: req.body.name,
+            description: req.body.description,
+            email: req.body.email,
+            ingredients: req.body.ingredients,
+            category: req.body.category,
+            image: newImageName
+        });
+
+        await newRecipe.save();
+
+        req.flash('infoSubmit', 'Recipe has been added.')
+        res.redirect('/submit-recipe');
+    } catch (error) {
+        // res.json(error);
+        req.flash('infoErrors', error);
+        res.redirect('/submit-recipe');
     }
-
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    sampleFile = req.files.image;
-    newImageName = Date.now() + sampleFile.name
-    uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName
-
-    // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv(uploadPath, function (err) {
-        if (err)
-            return res.status(500).send(err);
-    });
-
-    const newRecipe = new Recipe({
-        name: req.body.name,
-        description: req.body.description,
-        email: req.body.email,
-        ingredients: req.body.ingredients,
-        category: req.body.category,
-        image: req.body.image
-    });
-    newRecipe.save()
 }
+
 
 exports.getUpdateRecipe = (req, res, next) => {
     const recipeId = req.params.id
@@ -160,7 +172,7 @@ exports.getUpdateRecipe = (req, res, next) => {
 
 exports.postUpdateRecipe = (req, res, next) => {
     const recipeId = req.params.id
-    const updatedName= req.body.name
+    const updatedName = req.body.name
     const updatedDescription = req.body.description
     const updatedEmail = req.body.email
     const updatedIngredients = req.body.ingredients
@@ -188,23 +200,23 @@ exports.postUpdateRecipe = (req, res, next) => {
     });
 
     Recipe.findById(recipeId)
-    .then(recipe => {
-        recipe.name = updatedName;
-        recipe.description = updatedDescription;
-        recipe.email = updatedEmail;
-        recipe.ingredients = updatedIngredients;
-        recipe.category = updatedCategory
-        recipe.image = newImageName
+        .then(recipe => {
+            recipe.name = updatedName;
+            recipe.description = updatedDescription;
+            recipe.email = updatedEmail;
+            recipe.ingredients = updatedIngredients;
+            recipe.category = updatedCategory
+            recipe.image = newImageName
 
-        recipe.save()
-    })
-    .then(result => {
-        res.redirect(`/recipe/${recipeId}`)
-    })
-    .catch(err => console.log(err))
+            recipe.save()
+        })
+        .then(result => {
+            res.redirect(`/recipe/${recipeId}`)
+        })
+        .catch(err => console.log(err))
 }
 
-exports.get404 = (req,res,next) => {
+exports.get404 = (req, res, next) => {
     res.render('get404', {
         title: '404 Not Found',
     })
